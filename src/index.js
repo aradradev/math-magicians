ba74a356d22e6e5b4a27dc10bdbabb405b8a3e46
Abdoul <abdoulramanediallo44@gmail.com>
2023-07-03 11:13:59 +0000

    First Setup react added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const Main = () => <Section />;
+
+const Section = () => (
+  <div>
+    <Greeting />
+    <Paragraph />
+  </div>
+);
+
+const Greeting = () => <h2>Hello World</h2>;
+
+const Paragraph = () => <p>React setup</p>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
