402fcc58c6bc8dd8aba1b1629f3d6eb1eee70b66
Abdoul <abdoulramanediallo44@gmail.com>
2023-07-03 11:58:50 +0000

    add colors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const Section = () => (
 
 const Greeting = () => <h2>Hello World</h2>;
 
-const Paragraph = () => <p>React setup</p>;
+const Paragraph = () => <p>React setup component</p>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Main />);
