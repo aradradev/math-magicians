0a83e8ab2887cb9f2f5c68f80ff4d39121b3fee4
Abdoul <abdoulramanediallo44@gmail.com>
2023-07-12 08:50:09 +0000

    add navbar and link to each components in order to create a SPA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 const App = () => (
-  <div className="section">
-    <Calculator />
-    <Quote />
-  </div>
+  <>
+    <div className="navigation">
+      <Link style={{ textDecoration: 'none' }} to="/">
+        Math Magicians
+      </Link>
+      <Navbar />
+    </div>
+    <div className="section">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </div>
+  </>
 );
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const Main = () => <App />;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
